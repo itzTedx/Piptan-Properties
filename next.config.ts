88357c55854d03b0c5c5215fa074c43bcf40ae5b
Melwin <zomboi.offi@gmail.com>
2026-01-12 16:09:27 +0400

    Enhance Next.js configuration by adding typed routes and enabling filesystem caching for development and build processes.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+	/* config options here */
+	reactCompiler: true,
+
+	typedRoutes: true,
+
+	experimental: {
+		// Enable filesystem caching for `next dev`
+		turbopackFileSystemCacheForDev: true,
+		// Enable filesystem caching for `next build`
+		turbopackFileSystemCacheForBuild: true,
+	},
 };
 
 export default nextConfig;
